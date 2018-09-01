21aa75793ea05e57db07ab7249f72fef875321ae
G@SeS <very.nice@ukr.net>
2018-09-01 15:49:31 +0300

    Відображення всіх помилок та підключення автозагрузки через composer

--- Public/index.php
+++ Public/index.php
@@ -1,2 +1,9 @@
 <?php
-echo '<h1>Hello, World!</h1>';
+// Виводимо усі помилки на стадії розробки додатку
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Змінюємо каталог, щоб працювати з кереню додатку
+chdir(dirname(__DIR__));
+
+require_once 'vendor/autoload.php';
